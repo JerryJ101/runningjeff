3b1a20203cabaeeb06b5486a260d9c70141c7ebd
Cache Makers <Image15@iMac-L.local>
2017-03-14 18:00:27 -0600

    enemy developed

--- main.cpp
+++ main.cpp
@@ -11,9 +11,30 @@ int main()
 {
  sf::RenderWindow window(sf::VideoMode(WIDE,HIGH),"Adventure!");
 
-sf::CircleShape circle(100);
-circle.setFillColor(sf::Color::Blue);
-circle.setPosition(300,400);
+sf::Texture enemy_texture;
+if(!enemy_texture.loadFromFile("assets/ghost.png"))
+{
+  std::cout << "Error loading enemy texture!" << std::endl;
+}
+sf::Sprite enemy;
+enemy.setTexture(enemy_texture);
+enemy.setScale(sf::Vector2f(10,10));
+enemy.setPosition(100,200);
+
+
+sf::Font font;
+if(!font.loadFromFile("assets/ARCADECLASSIC.TTF"))
+{
+  std::cout << "Error loading font!" << std::endl;
+}
+sf::Text livesDisp;
+livesDisp.setFont(font);
+livesDisp.setCharacterSize(24);
+livesDisp.setColor(sf::Color::Black);
+livesDisp.setPosition(100,100);
+
+
+
 
 sf::Texture hero_texture;
 hero_texture.loadFromFile("assets/blobby.png");
@@ -21,16 +42,32 @@ hero_texture.loadFromFile("assets/blobby.png");
   std::cout << "Error loading hero texture!" << std::endl;
 }
 
+
 sf::Sprite hero;
 hero.setTexture(hero_texture);
 hero.setScale(sf::Vector2f(10,10));
+hero.setPosition(200,200);
+
+sf::Texture wall_texture;
+if(!wall_texture.loadFromFile("assets/stone_wall.png"))
+{
+  std::cout << "Error loading wall texture!" << std::endl;
+}
+
+sf::Sprite wall;
+wall.setTexture(wall_texture);
+wall.setScale(sf::Vector2f(2,2));
+wall.setPosition(500,300);
 
 
-int circleDirection = 0;
+int enemyDirection = 0;
 int heroSpeed = 1;
 
 
 int points =10;
+
+int lives =5;
+bool takingDamage = false;
 
 //Game Loop
   while(window.isOpen())
@@ -44,77 +81,86 @@ int points =10;
     {
       window.close();
     }
-    if(event.type == sf::Event::KeyPressed)
-    {
-      if(event.key.code == sf::Keyboard::Up)
-      {
-        hero.move(0,-heroSpeed);
-      }
-      if(event.key.code == sf::Keyboard::Down)
-      {
-        hero.move(0,heroSpeed);
-      }
-      if(event.key.code == sf::Keyboard::Right)
-      {
-        hero.move(heroSpeed,0);
-      }
-      if(event.key.code == sf::Keyboard::Left)
-      {
-        hero.move(-heroSpeed,0);
-      }
-    }
   }
 
 
   if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && hero.getPosition().y > 0)
   {
     hero.move(0,-heroSpeed);
+    if(hero.getGlobalBounds().intersects(wall.getGlobalBounds()))
+    {
+      hero.move(0,heroSpeed);
+    }
   }
   if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down) && hero.getPosition().y < HIGH-TILESIZE)
   {
     hero.move(0,heroSpeed);
+    if(hero.getGlobalBounds().intersects(wall.getGlobalBounds()))
+    {
+      hero.move(0,-heroSpeed);
+    }
   }
   if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right) && hero.getPosition().x < WIDE-TILESIZE)
   {
     hero.move(heroSpeed,0);
+    if(hero.getGlobalBounds().intersects(wall.getGlobalBounds()))
+    {
+      hero.move(-heroSpeed,0);
+    }
   }
   if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left) && hero.getPosition().x > 0)
   {
     hero.move(-heroSpeed,0);
+    if(hero.getGlobalBounds().intersects(wall.getGlobalBounds()))
+    {
+      hero.move(heroSpeed,0);
+    }
   }
 
-if(hero.getGlobalBounds().intersects(circle.getGlobalBounds()))
+if(hero.getGlobalBounds().intersects(enemy.getGlobalBounds()))
+
 {
-  std::cout << "Touching!" << std::endl;
+  if(takingDamage ==false)
+  {
+    lives --;
+    takingDamage = true;
+  }
 }
 else
 {
-  std::cout << "Not Touching!" << std::endl;
+  takingDamage = false;
 }
+
+
+
+std::cout << lives << std::endl;
+livesDisp.setString(std::to_string(lives));
 
 
   window.clear(sf::Color::Green);
 //update stuff - This is Aaron
-if(circleDirection == 0)
+if(enemyDirection == 0)
 {
-  circle.move(0.5,0);
+  enemy.move(0.5,0);
 }
 else
 {
-  circle.move(-0.5,0);
+  enemy.move(-0.5,0);
 }
-if(circle.getPosition().x > 600)
+if(enemy.getPosition().x > 600)
 {
-  circleDirection = 1;
+  enemyDirection = 1;
 }
-if(circle.getPosition().x < 0)
+if(enemy.getPosition().x < 0)
 {
-  circleDirection = 0;
+  enemyDirection = 0;
 }
 //render - This is Max
-window.draw(circle);
+window.draw(enemy);
+window.draw(wall);
 window.draw(hero);
-  window.display();
+window.draw(livesDisp);
+window.display();
 
 
  }
