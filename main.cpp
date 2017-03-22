f24b2a3922546d3ee111d1d37b49b90602d8e58e
Cache Makers <Image15@iMac-L.local>
2017-03-21 18:12:21 -0600

    not quite done today

--- main.cpp
+++ main.cpp
@@ -1,15 +1,66 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <vector>
+
 
 #define WIDE 800
-#define HIGH 600
+#define HIGH 640
 
-#define TILESIZE 160
+#define TILESIZE 32
 
+bool textColl(sf::Sprite a, sf::Sprite b)
+{
+  return a.getGlobalBounds().intersects(b.getGlobalBounds());
+}
+
+
+
+bool testWallColl(sf::Sprite, std::vector<sf::Sprite> walls)
+{
+  for(int i=0; i< walls.size(); i++)
+  {
+    if(textColl())
+    {
+      return true;
+    }
+  }
+  return false;
+}
 
 int main()
 {
  sf::RenderWindow window(sf::VideoMode(WIDE,HIGH),"Adventure!");
+ int tileW = WIDE/TILESIZE;
+ int tileH = HIGH/TILESIZE;
+
+ std::vector<std::vector<bool> > map;
+
+ for(int i=0; i<tileH; i++)
+ {
+   std::vector<bool> temp;
+   for(int j=0; j<tileH; j++)
+   {
+     temp.push_back(false);
+   }
+   map.push_back(temp);
+ }
+ map[12][3] = true;
+
+ for(int i=0; i<map.size(); i++)
+ {
+   for(int j=0; j<map[i].size(); j++)
+   {
+     if(map[i][j] == true)
+     {
+      std::cout << '0';
+     }
+     else
+     {
+       std::cout << ' ';
+     }
+   }
+   std::cout << std::endl;
+ }
 
 sf::Texture enemy_texture;
 if(!enemy_texture.loadFromFile("assets/ghost.png"))
@@ -18,7 +69,7 @@ if(!enemy_texture.loadFromFile("assets/ghost.png"))
 }
 sf::Sprite enemy;
 enemy.setTexture(enemy_texture);
-enemy.setScale(sf::Vector2f(10,10));
+enemy.setScale(sf::Vector2f(2,2));
 enemy.setPosition(100,200);
 
 
@@ -31,13 +82,13 @@ sf::Text livesDisp;
 livesDisp.setFont(font);
 livesDisp.setCharacterSize(24);
 livesDisp.setColor(sf::Color::Black);
-livesDisp.setPosition(100,100);
+livesDisp.setPosition(50,50);
 
 
 
 
 sf::Texture hero_texture;
-hero_texture.loadFromFile("assets/blobby.png");
+if(!hero_texture.loadFromFile("assets/blobby.png"))
 {
   std::cout << "Error loading hero texture!" << std::endl;
 }
@@ -45,7 +96,7 @@ hero_texture.loadFromFile("assets/blobby.png");
 
 sf::Sprite hero;
 hero.setTexture(hero_texture);
-hero.setScale(sf::Vector2f(10,10));
+hero.setScale(sf::Vector2f(4,4));
 hero.setPosition(200,200);
 
 sf::Texture wall_texture;
@@ -54,10 +105,35 @@ if(!wall_texture.loadFromFile("assets/stone_wall.png"))
   std::cout << "Error loading wall texture!" << std::endl;
 }
 
-sf::Sprite wall;
-wall.setTexture(wall_texture);
-wall.setScale(sf::Vector2f(2,2));
-wall.setPosition(500,300);
+
+
+std::vector<sf::Sprite> wallArr;
+for(int i=0; i<map.size(); i++)
+{
+  for(int j=0; j<map[i].size(); j++)
+  {
+    if(map[i][j] == true)
+    {
+    sf::Sprite temp;
+    temp.setTexture(wall_texture);
+    temp.setScale(sf::Vector2f(2,2));
+    temp.setPosition(j*TILESIZE,i*TILESIZE);
+    wallArr.push_back(temp);
+    }
+  }
+}
+
+sf::Texture grass_texture;
+if(!grass_texture.loadFromFile("assets/grass.png"))
+{
+  std::cout << "Error loading grass texture!" << std::endl;
+}
+sf::Sprite grass;
+grass.setTexture(grass_texture);
+grass.setScale(sf::Vector2f(2,2));
+
+
+
 
 
 int enemyDirection = 0;
@@ -87,7 +163,7 @@ bool takingDamage = false;
   if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && hero.getPosition().y > 0)
   {
     hero.move(0,-heroSpeed);
-    if(hero.getGlobalBounds().intersects(wall.getGlobalBounds()))
+    if(testWallColl(hero, wallArr))
     {
       hero.move(0,heroSpeed);
     }
@@ -95,7 +171,7 @@ bool takingDamage = false;
   if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down) && hero.getPosition().y < HIGH-TILESIZE)
   {
     hero.move(0,heroSpeed);
-    if(hero.getGlobalBounds().intersects(wall.getGlobalBounds()))
+    if(testWallColl(hero, wallArr))
     {
       hero.move(0,-heroSpeed);
     }
@@ -103,7 +179,7 @@ bool takingDamage = false;
   if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right) && hero.getPosition().x < WIDE-TILESIZE)
   {
     hero.move(heroSpeed,0);
-    if(hero.getGlobalBounds().intersects(wall.getGlobalBounds()))
+    if(testWallColl(hero, wallArr))
     {
       hero.move(-heroSpeed,0);
     }
@@ -111,7 +187,7 @@ bool takingDamage = false;
   if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left) && hero.getPosition().x > 0)
   {
     hero.move(-heroSpeed,0);
-    if(hero.getGlobalBounds().intersects(wall.getGlobalBounds()))
+    if(testWallColl(hero, wallArr))
     {
       hero.move(heroSpeed,0);
     }
@@ -133,7 +209,7 @@ else
 
 
 
-std::cout << lives << std::endl;
+//std::cout << lives << std::endl;
 livesDisp.setString(std::to_string(lives));
 
 
@@ -156,8 +232,20 @@ if(enemy.getPosition().x < 0)
   enemyDirection = 0;
 }
 //render - This is Max
+for(int i=0; i<tileH ; i++)
+{
+  for(int j=0; j<tileW ; j++)
+  {
+    grass.setPosition(j*TILESIZE, i*TILESIZE);
+    window.draw(grass);
+  }
+}
+
 window.draw(enemy);
-window.draw(wall);
+for(int i=0; i<wallArr.size(); i++)
+{
+  window.draw(wallArr[i]);
+}
 window.draw(hero);
 window.draw(livesDisp);
 window.display();
